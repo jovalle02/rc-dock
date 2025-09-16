d1446bbf91c828dbc57d3dcea1701dcea43caa48
jovalle02 <ja.ovalle2@uniandes.edu.co>
2025-09-16 18:55:29 -0500

    Add built distribution files

---
+++ es/DockData.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+/** @ignore */
+export const defaultGroup = {
+    floatable: true,
+    maximizable: true,
+};
+/** @ignore */
+export const placeHolderStyle = 'place-holder';
+/** @ignore */
+export const maximePlaceHolderId = '-maximized-placeholder-';
+/** @ignore */
+export const placeHolderGroup = {
+    floatable: false,
+};
+/** @ignore */
+export const DockContextType = React.createContext(null);
+/** @ignore */
+export const DockContextProvider = DockContextType.Provider;
+/** @ignore */
+export const DockContextConsumer = DockContextType.Consumer;
